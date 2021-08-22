4636d77d2c86e71f19acf258555c8441d4c53a9b
R-Sugimoto-0718 <wasp2123321@gmail.com>
2021-08-22 18:01:25 +0900

    絶対パス設定

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
